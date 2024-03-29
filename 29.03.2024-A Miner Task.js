502956de9ad6b08b97debd436fddf5dd5b259d13
Miroslav Tsarev <mtzarev@gmail.com>
2024-03-29 23:33:49 +0200

    Add files via upload

---
+++ 29.03.2024-A Miner Task.js
@@ -0,0 +1,17 @@
+function minerTask(input) {
+    let mineCollection = {};
+    for (let i = 0; i < input.length; i += 2) {
+      let resource = input[i];
+      let qty = Number(input[i + 1]);
+      if (!mineCollection.hasOwnProperty(resource))
+        mineCollection[resource] = qty;
+      else {
+        mineCollection[resource] += qty;
+      }
+    }
+    for (let resource in mineCollection) {
+      console.log(`${resource} -> ${mineCollection[resource]}`);
+    }
+  }
+  
+  minerTask(["gold", "155", "silver", "10", "copper", "17", "gold", "15"]);
